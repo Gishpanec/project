cae5840973ca7924ff1566c6dcd858ce2733efe3
Islam <dzuganov.islam@aplana.com>
2020-07-13 20:18:11 +0300

    отладка
    Signed-off-by: Islam <dzuganov.islam@aplana.com>

--- src/test2.java
+++ src/test2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class test2 {
-    public static void main(String[] args) {
+    public static void test2(String[] args) {
 
         Scanner in = new Scanner(System.in);
         /*отображение введенного числа*/
--- src/test3.java
+++ src/test3.java
@@ -1,17 +1,17 @@
-public class test3 {
+class test3 {
 
     /*Расчет числа кубических дюймов в кубе объемом в 1 куб. милю*/
 
-    class Inches{
-        public static void test3(String args[]) {
-            long ci;
-            long im;
-            im = 5280*12;
-            ci = im*im*im;
-
-            System.out.println("В одной кубической миле содержится" + ci + "кубических дюймов");
-
-        }
-
-    }
-}
+ class Inches{
+//        public static void main(String[] args) {
+//            long ci;
+//            long im;
+//            im = 5280*12;
+//            ci = im*im*im;
+//
+//            System.out.println("В одной кубической миле содержится" + ci + "кубических дюймов");
+//
+//        }
+//
+//    }
+//}
