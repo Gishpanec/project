9d70795044d52a7259f107f4eb820d688165c015
Islam <dzuganov.islam@aplana.com>
2020-07-14 15:55:29 +0300

    пример работы шифровки/дешифровки текста
    Signed-off-by: Islam <dzuganov.islam@aplana.com>

--- src/test6.java
+++ src/test6.java
@@ -1,26 +1,36 @@
 public class test6 {
-    // метод для шифровки текста с помощью XOR
-    public static byte[] encode(String secret, String key) {
-        byte[] btxt =null;
-        byte[] bkey = null;
+    public static void main(String[] args) {
+        {
 
-        btxt = secret.getBytes();
-        bkey = key.getBytes();
+        // метод для шифровки текста с помощью XOR
 
-        byte[] result = new byte[secret.length()];
-        for (int i =0; i<btxt.length; i++) {
-            result[i] = (byte) (btxt[i] ^ bkey[i % bkey.length]);
+        public static byte[] encode (String secret, String key){
+
+            byte[] btxt = null;
+            byte[] bkey = null;
+
+            btxt = secret.getBytes();
+            bkey = key.getBytes();
+
+
+            byte[] result = new byte[secret.length()];
+
+            for (int i = 0; i < btxt.length; i++) {
+                result[i] = (byte) (btxt[i] ^ bkey[i % bkey.length]);
+
+            }
+            return result;
         }
-        return result;
-    }
-    //метод расшифровки текста
-    public static String decode(byte[]secret,String key){
-        byte[] result = new byte[secret.length];
-        byte[] bkey = key.getBytes();
+// метод для расшифровки текста
 
-        for (int i=0; i<secret.length; i++){
-            result[i] = (byte)(secret[i]^ bkey[i%bkey.length]);
+        public static String decode ( byte[] secret, String key){
+            byte[] result = new byte[secret.length];
+            byte[] bkey = key.getBytes();
+
+            for (int i = 0; i < secret.length; i++) {
+
+                result[i] = (byte) (secret[i] ^ bkey[i % bkey.length]);
+            }
+            return new String(result);
         }
-        return new String(result);
     }
-}
