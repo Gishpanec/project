280ba8b02c5f04498c32941ea7ed619499a85d06
Islam <dzuganov.islam@aplana.com>
2020-07-17 16:22:47 +0300

    Таблица Умножения без использования цикла
    Signed-off-by: Islam <dzuganov.islam@aplana.com>

--- src/table.java
+++ src/table.java
@@ -1,15 +1,107 @@
+import java.util.Scanner;
+
 public class table {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         int x;
         int y;
-        for (int i=1;i<10;i++){
+        for (int i = 1; i < 10; i++) {
             String str = "";
-            for (int j=1;j<10;j++){
-                str+=(Integer.toString(i)+"*"+Integer.toString(j)+"="+Integer.toString(i+j)+"\t");
+            for (int j = 1; j < 10; j++) {
+                str += (Integer.toString(i) + "*" + Integer.toString(j) + "=" + Integer.toString(i + j) + "\t");
             }
             System.out.println(str);
 
 
         }
     }
+
+    static class multitable {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int otvet, a, post;
+            boolean result;
+
+            // ввод значения для переменных а,в
+            System.out.print("введите любое число: ");
+            int x = in.nextInt();
+            String str = "";
+            a = 0;
+
+            post = a + 1;
+            otvet = x * post;
+            result = post <= 10;
+            {
+                str = (Integer.toString(x) + "*" + Integer.toString(post) + "=" + Integer.toString(otvet) + "\t");
+                System.out.println(str);
+            }
+            post = a + 2;
+            otvet = x * post;
+            result = post <= 10;
+            {
+                str = (Integer.toString(x) + "*" + Integer.toString(post) + "=" + Integer.toString(otvet) + "\t");
+                System.out.println(str);
+            }
+            post = a + 3;
+            otvet = x * post;
+            result = post <= 10;
+            {
+                str = (Integer.toString(x) + "*" + Integer.toString(post) + "=" + Integer.toString(otvet) + "\t");
+                System.out.println(str);
+            }
+            post = a + 4;
+            otvet = x * post;
+            result = post <= 10;
+            {
+                str = (Integer.toString(x) + "*" + Integer.toString(post) + "=" + Integer.toString(otvet) + "\t");
+                System.out.println(str);
+            }
+            post = a + 5;
+            otvet = x * post;
+            result = post <= 10;
+            {
+                str = (Integer.toString(x) + "*" + Integer.toString(post) + "=" + Integer.toString(otvet) + "\t");
+                System.out.println(str);
+            }
+            post = a + 6;
+            otvet = x * post;
+            result = post <= 10;
+            {
+                str = (Integer.toString(x) + "*" + Integer.toString(post) + "=" + Integer.toString(otvet) + "\t");
+                System.out.println(str);
+            }
+            post = a + 7;
+            otvet = x * post;
+            result = post <= 10;
+            {
+                str = (Integer.toString(x) + "*" + Integer.toString(post) + "=" + Integer.toString(otvet) + "\t");
+                System.out.println(str);
+            }
+            post = a + 8;
+            otvet = x * post;
+            result = post <= 10;
+            {
+                str = (Integer.toString(x) + "*" + Integer.toString(post) + "=" + Integer.toString(otvet) + "\t");
+                System.out.println(str);
+            }
+            post = a + 9;
+            otvet = x * post;
+            result = post <= 10;
+            {
+                str = (Integer.toString(x) + "*" + Integer.toString(post) + "=" + Integer.toString(otvet) + "\t");
+                System.out.println(str);
+            }
+            post = a + 10;
+            otvet = x * post;
+            result = post <= 10;
+            {
+                str = (Integer.toString(x) + "*" + Integer.toString(post) + "=" + Integer.toString(otvet) + "\t");
+                System.out.println(str);
+            }
+
+
+        }
+    }
 }
+
+
+
